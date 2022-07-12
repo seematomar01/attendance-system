1efd983d6f2e4d960625ba7002b39d10c89a63ae
seematomar01 <86758201+seematomar01@users.noreply.github.com>
2022-07-12 11:36:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,109 @@
+from flask import Flask, render_template, Response
+import cv2
+import cv2
+import numpy as np
+import face_recognition as face_rec
+import os
+from datetime import  datetime
+path = 'employee images'
+employeeImg = []
+employeeName = []
+myList = os.listdir(path)
+engine = textSpeach.init()
+
+def resize(img, size) :
+    width = int(img.shape[1]*size)
+    height = int(img.shape[0] * size)
+    dimension = (width, height)
+    return cv2.resize(img, dimension, interpolation= cv2.INTER_AREA)
+
+
+
+def findEncoding(images) :
+    imgEncodings = []
+    for img in images :
+        img = resize(img, 0.50)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encodeimg = face_rec.face_encodings(img)[0]
+        imgEncodings.append(encodeimg)
+    return imgEncodings
+def MarkAttendence(name):
+    with open('attendence.csv', 'r+') as f:
+        myDatalist =  f.readlines()
+        nameList = []
+        for line in myDatalist :
+            entry = line.split(',')
+            nameList.append(entry[0])
+
+        if name not in nameList:
+            now = datetime.now()
+            timestr = now.strftime('%H:%M')
+            f.writelines(f'\n{name}, {timestr}')
+            statment = str('welcome to seasia' + name)
+
+for cl in myList :
+    curimg = cv2.imread(f'{path}/{cl}')
+    employeeImg.append(curimg)
+    employeeName.append(os.path.splitext(cl)[0])
+
+EncodeList = findEncoding(employeeImg)
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)  # use 0 for web camera
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read()
+        #print(frame)
+        
+        
+
+        facesInFrame = face_rec.face_locations(frame)
+        encodeFacesInFrame = face_rec.face_encodings(frame, facesInFrame)
+        
+
+        for encodeFace, faceloc in zip(encodeFacesInFrame, facesInFrame) :
+            matches = face_rec.compare_faces(EncodeList, encodeFace)
+            facedis = face_rec.face_distance(EncodeList, encodeFace)
+            print(facedis)
+            if min(facedis) < 0.5:
+                matchIndex = np.argmin(facedis)
+
+                print(matchIndex)
+
+
+                name = employeeName[matchIndex].upper()
+                y1, x2, y2, x1 = faceloc
+                y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                cv2.rectangle(frame, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                MarkAttendence(name)
+                print(name)
+
+        
+        
+        
+        
+        
+        
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
